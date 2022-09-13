f532b8b325071c8f9fdfed5fb0cfe71fd6587468
Romina <romina.pati@gmail.com>
2022-09-13 10:27:31 -0400

    query and update animals table

--- queries.sql
+++ queries.sql
@@ -24,3 +24,24 @@ SELECT * from animals WHERE name NOT LIKE 'Gabumon';
 
 -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+
+-- second milestone
+
+-- How many animals are there?
+SELECT COUNT(*) from animals;
+
+-- How many animals have never tried to escape?
+SELECT COUNT(*) from animals WHERE escape_attempts = 0;
+
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) from animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT SUM(escape_attempts), neutered from animals GROUP BY neutered;
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT MIN(weight_kg), MAX(weight_kg), species from animals GROUP BY species;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT AVG(escape_attempts), species from animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
