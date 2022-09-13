a6682b657ade215016e69c89361152fa2b22c582
Romina <romina.pati@gmail.com>
2022-09-13 15:43:03 -0400

    query multiple tables

--- queries.sql
+++ queries.sql
@@ -56,3 +56,52 @@ SELECT MIN(weight_kg), MAX(weight_kg), species from animals GROUP BY species;
 
 -- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
 SELECT AVG(escape_attempts), species from animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+
+
+
+-- THIRD MILESTONE
+
+
+-- What animals belong to Melody Pond?
+SELECT A.name AS animal_name, O.full_name AS owner_name
+FROM animals A
+JOIN owners O ON A.owner_id = O.id
+WHERE O.full_name = 'Melody Pond';
+
+-- List of all animals that are pokemon (their type is Pokemon).
+SELECT A.name AS animal_name, S.name AS type
+FROM animals A
+JOIN species S ON A.species_id = S.id
+WHERE S.name = 'Pokemon';
+
+-- List all owners and their animals, remember to include those that don't own any animal.
+SELECT O.full_name AS owner_name, A.name AS animal_name
+FROM owners O
+LEFT JOIN animals A ON A.owner_id = O.id
+WHERE A.name IS NULL OR A.name IS NOT NULL;
+
+-- How many animals are there per species?
+SELECT COUNT(S.name) AS total_animals, S.name AS specie_name
+FROM animals A
+JOIN species S ON A.species_id = S.id
+GROUP BY S.name;
+
+-- List all Digimon owned by Jennifer Orwell.
+SELECT A.name AS animal_name, O.full_name AS owner_name
+FROM animals A
+JOIN owners O ON A.owner_id = O.id
+WHERE A.species_id = 2 AND O.full_name = 'Jennifer Orwell';
+
+-- List all animals owned by Dean Winchester that haven't tried to escape.
+SELECT A.name AS animal_name, O.full_name AS owner_name
+FROM animals A
+JOIN owners O ON A.owner_id = O.id
+WHERE A.escape_attempts = 0 AND O.full_name = 'Dean Winchester';
+
+-- Who owns the most animals?
+SELECT COUNT(A.name) AS total_animals, O.full_name AS owner_name
+FROM animals A
+RIGHT JOIN owners O ON A.owner_id = O.id
+GROUP BY O.full_name
+HAVING COUNT(A.name) IS NULL OR COUNT(A.name) IS NOT NULL
+ORDER BY COUNT(O.full_name) DESC;
