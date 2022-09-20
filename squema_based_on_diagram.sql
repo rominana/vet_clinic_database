8460754a839f5496020da584fae76f1be27f4914
Yilikal250 <teshomeyilikal250@gmail.com>
2022-09-20 18:09:22 +0300

    Created invoice_items,invoice and join tables using a pgAdmin

--- squema_based_on_diagram.sql
+++ squema_based_on_diagram.sql
@@ -20,29 +20,67 @@ CREATE TABLE treatments (
   name VARCHAR(50)
 );
 
-CREATE TABLE invoice_items (
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  unit_price DECIMAL,
-  quantity INT,
-  total_price DECIMAL,
-  invoice_id INT,
-  treatment_id INT,
-  FOREIGN KEY (treatment_id) REFERENCES treatments (id) ON DELETE CASCADE,
-  FOREIGN KEY (invoice_id) REFERENCES invoices (id) ON DELETE CASCADE
-);
-
-CREATE TABLE invoices (
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  total_amount DECIMAL,
-  generated_at TIMESTAMP,
-  payer_at TIMESTAMP,
-  medical_history_id INT,
-  FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id) ON DELETE CASCADE
-);
-
-CREATE TABLE jointable (
-  treatment_id INT,
-  medical_history_id INT,
-  FOREIGN KEY treatment_id REFERENCES treatments (id) ON DELETE CASCADE,
-  FOREIGN KEY medical_history_id REFERENCES medical_histories (id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS public.invoice_items
+(
+    id integer NOT NULL,
+    unit_price numeric NOT NULL,
+    quantity integer NOT NULL,
+    total_price numeric NOT NULL,
+    invoice_id integer NOT NULL,
+    treatment_id integer NOT NULL,
+    CONSTRAINT invoice_items_pkey PRIMARY KEY (id),
+    CONSTRAINT invoice_id FOREIGN KEY (invoice_id)
+        REFERENCES public.invoices (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT treatment_id FOREIGN KEY (treatment_id)
+        REFERENCES public.treatments (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
 )
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.invoice_items
+    OWNER to postgres;
+
+CREATE TABLE IF NOT EXISTS public.invoices
+(
+    id integer NOT NULL,
+    total_amount numeric NOT NULL,
+    generate_at timestamp with time zone NOT NULL,
+    payed_at timestamp with time zone NOT NULL,
+    medical_histories_id integer NOT NULL,
+    CONSTRAINT invoices_pkey PRIMARY KEY (id),
+    CONSTRAINT medical_histories_id FOREIGN KEY (medical_histories_id)
+        REFERENCES public.medical_histories (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.invoices
+    OWNER to postgres;
+
+CREATE TABLE IF NOT EXISTS public."joinTable"
+(
+    medical_histories_id integer NOT NULL,
+    treatment_id integer NOT NULL,
+    CONSTRAINT medical_histories_id FOREIGN KEY (medical_histories_id)
+        REFERENCES public.medical_histories (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT treatment_id FOREIGN KEY (treatment_id)
+        REFERENCES public.treatments (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."joinTable"
+    OWNER to postgres;
